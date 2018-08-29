27bf5fe6abd5b8a99aca5b5176a665ae6ef72187
David Roundy <roundyd@physics.oregonstate.edu>
2018-08-29 11:23:31 -0700

    start making a template do something

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate askama;
 extern crate serde_derive;
 extern crate serde;
 
-use actix_web::{server, http::Method, App, Path, Result};
+use actix_web::{server, http::Method, App, Path, Result, HttpRequest, HttpResponse};
 use askama::Template;
 
 #[derive(Template, Serialize, Deserialize)]
@@ -14,15 +14,39 @@ struct HelloTemplate {
     name: String,
     apples: u32,
 }
-
-fn index(hello: Path<(HelloTemplate)>) -> Result<String> {
+fn hello(hello: Path<(HelloTemplate)>) -> Result<String> {
     Ok(hello.render().unwrap())
 }
 
+#[derive(Template, Serialize, Deserialize)]
+#[template(path = "day.html")]
+struct Day {
+    id: u64,
+}
+
+#[derive(Template, Serialize, Deserialize)]
+#[template(path = "index.html")]
+struct Index {
+    days: Vec<Day>,
+}
+fn index(_req: HttpRequest) -> HttpResponse {
+    let mut index = Index {
+        days: Vec::new(),
+    };
+    for i in 1..20 {
+        index.days.push(Day { id:  i });
+    }
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(index.render().unwrap())
+}
+
 fn main() {
+    println!("I am running now!!!");
     server::new(|| App::new()
-                .resource("/{name}/{apples}/index.html",
-                          |r| r.method(Method::GET).with(index))
+                .route("/index.html", Method::GET, index)
+                .resource("/hello/{name}/{apples}/index.html",
+                          |r| r.method(Method::GET).with(hello))
                 )
         .bind("127.0.0.1:8088")
         .unwrap()
