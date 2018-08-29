ce0c854623b33570c2c89d3fd4c683e51d866bc2
David Roundy <roundyd@physics.oregonstate.edu>
2018-08-29 11:46:49 -0700

    move towards actual data model

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,7 @@ fn hello(hello: Path<(HelloTemplate)>) -> Result<String> {
 #[template(path = "day.html")]
 struct Day {
     id: u64,
+    pairings: Vec<Pairing>,
 }
 
 #[derive(Template, Serialize, Deserialize)]
@@ -29,12 +30,46 @@ struct Day {
 struct Index {
     days: Vec<Day>,
 }
+
+#[derive(Serialize, Deserialize)]
+struct Student {
+    id: u64,
+}
+#[derive(Serialize, Deserialize)]
+struct Section {
+    id: u64,
+}
+#[derive(Serialize, Deserialize)]
+struct Team {
+    id: u64,
+}
+#[derive(Serialize, Deserialize)]
+enum Pairing {
+    Pairing {
+        section: Section,
+        team: Team,
+        primary: Student,
+        secondary: Option<Student>,
+    },
+    Absent(Student),
+}
+
+#[derive(Serialize, Deserialize)]
+struct Database {
+    days: Vec<Day>,
+    students: Vec<Student>,
+    teams: Vec<Team>,
+}
+
 fn index(_req: HttpRequest) -> HttpResponse {
     let mut index = Index {
         days: Vec::new(),
     };
     for i in 1..20 {
-        index.days.push(Day { id:  i });
+        index.days.push(Day {
+            id:  i,
+            pairings: Vec::new(),
+        });
     }
     HttpResponse::Ok()
         .content_type("text/html")
@@ -45,6 +80,12 @@ fn main() {
     println!("I am running now!!!");
     server::new(|| App::new()
                 .route("/index.html", Method::GET, index)
+                .route("/", Method::GET, index)
+                .route("/day-{day}", Method::GET, index)
+                .route("/grid-{day}", Method::GET, index)
+                .route("/students", Method::GET, index)
+                .route("/sections", Method::GET, index)
+                .route("/teams", Method::GET, index)
                 .resource("/hello/{name}/{apples}/index.html",
                           |r| r.method(Method::GET).with(hello))
                 )
