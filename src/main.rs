4237dad944c3e74bedb5d74d844678a828f15e49
David Roundy <roundyd@physics.oregonstate.edu>
2018-08-29 10:57:24 -0700

    get serde working for nicer path extraction

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 extern crate actix_web;
-use actix_web::{server, http::Method, App, Path, Result};
-
 #[macro_use]
 extern crate askama;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+
+use actix_web::{server, http::Method, App, Path, Result};
 use askama::Template;
 
-#[derive(Template)]
+#[derive(Template, Serialize, Deserialize)]
 #[template(path = "hello.html")]
 struct HelloTemplate {
     name: String,
     apples: u32,
 }
 
-fn index(info: Path<(String, u32)>) -> Result<String> {
-    let hello = HelloTemplate { name: info.0.clone(), apples: info.1 };
+fn index(hello: Path<(HelloTemplate)>) -> Result<String> {
     Ok(hello.render().unwrap())
 }
 
 fn main() {
     server::new(|| App::new()
-                .resource("/{username}/{id}/index.html",
+                .resource("/{name}/{apples}/index.html",
                           |r| r.method(Method::GET).with(index))
                 )
         .bind("127.0.0.1:8088")
