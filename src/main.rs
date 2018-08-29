f0af2001a73626177bd15c174a477951e500c8c0
David Roundy <roundyd@physics.oregonstate.edu>
2018-08-29 16:26:17 -0700

    a couple more pages

--- src/main.rs
+++ src/main.rs
@@ -36,13 +36,17 @@ struct Student {
     id: usize,
     name: String,
 }
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Template, Serialize, Deserialize, Clone, Debug)]
+#[template(path = "student.html")]
 struct Section {
     id: usize,
+    name: String,
 }
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Template, Serialize, Deserialize, Clone, Debug)]
+#[template(path = "student.html")]
 struct Team {
     id: usize,
+    name: String,
 }
 #[derive(Serialize, Deserialize, Clone)]
 enum Pairing {
@@ -96,40 +100,22 @@ struct Students {
     students: Vec<Student>,
 }
 
+#[derive(Template, Serialize, Deserialize)]
+#[template(path = "sections.html")]
+struct Sections {
+    sections: Vec<Section>,
+}
+
+#[derive(Template, Serialize, Deserialize)]
+#[template(path = "teams.html")]
+struct Teams {
+    teams: Vec<Team>,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 struct NewStudent {
     name: String,
 }
-// fn new_student(form:  Form<NewStudent>) -> HttpResponse {
-//     println!("{:?}", form);
-//     let mut database = Database::new();
-//     let student = Student {
-//         id: database.students.len(),
-//         name: form.name.clone(),
-//     };
-//     database.students.push(student);
-//     database.save();
-//     let page = Students {
-//         students: database.students.clone(),
-//     };
-//     HttpResponse::Ok()
-//         .content_type("text/html")
-//         .body(page.render().unwrap())
-// }
-
-// fn change_student(form:  Form<Student>) -> HttpResponse {
-//     println!("change {:?}", form);
-//     let mut database = Database::new();
-//     database.students[form.id].name = form.name.clone();
-//     database.save();
-//     let page = Students {
-//         students: database.students.clone(),
-//     };
-//     HttpResponse::Ok()
-//         .content_type("text/html")
-//         .body(page.render().unwrap())
-// }
-
 fn main() {
     println!("I am running now!!!");
     rouille::start_server("0.0.0.0:8088", move |request| {
@@ -179,6 +165,80 @@ fn main() {
                 };
                 page.render().unwrap()
             },
+            (GET) (/sections) => {
+                let page = Sections {
+                    sections: database.sections.clone(),
+                };
+                page.render().unwrap()
+            },
+            (POST) (/sections) => {
+                // let data = rouille::input::post::raw_urlencoded_post_input(request);
+                // println!("data is {:?}", data);
+                match post_input!(request, {
+                    id: usize,
+                    name: String,
+                }) {
+                    Ok(input) => {
+                        if input.id < database.sections.len() {
+                            database.sections[input.id] = Section {
+                                id: input.id,
+                                name: input.name,
+                            };
+                        } else {
+                            let section = Section {
+                                id: database.sections.len(),
+                                name: input.name,
+                            };
+                            database.sections.push(section);
+                        }
+                    }
+                    Err(e) => {
+                        return Response::text(format!("Post sections error: {:?}\n\n{:?}",
+                                                      request, e));
+                    }
+                }
+                let page = Sections {
+                    sections: database.sections.clone(),
+                };
+                page.render().unwrap()
+            },
+            (GET) (/teams) => {
+                let page = Teams {
+                    teams: database.teams.clone(),
+                };
+                page.render().unwrap()
+            },
+            (POST) (/teams) => {
+                // let data = rouille::input::post::raw_urlencoded_post_input(request);
+                // println!("data is {:?}", data);
+                match post_input!(request, {
+                    id: usize,
+                    name: String,
+                }) {
+                    Ok(input) => {
+                        if input.id < database.teams.len() {
+                            database.teams[input.id] = Team {
+                                id: input.id,
+                                name: input.name,
+                            };
+                        } else {
+                            let team = Team {
+                                id: database.teams.len(),
+                                name: input.name,
+                            };
+                            database.teams.push(team);
+                        }
+                    }
+                    Err(e) => {
+                        return Response::text(format!("Post teams error: {:?}\n\n{:?}",
+                                                      request, e));
+                    }
+                }
+                let page = Teams {
+                    teams: database.teams.clone(),
+                };
+                page.render().unwrap()
+            },
             _ => {
                 format!("Error: {:?}", request)
             },
@@ -186,26 +246,4 @@ fn main() {
         database.save();
         Response::html(html)
     });
-    // server::new(|| App::new()
-    //             // .middleware(
-    //             //     csrf::CsrfFilter::build()
-    //             //         .allowed_origin("https://www.example.com")
-    //             //         .finish())
-    //             .route("/index.html", Method::GET, index)
-    //             .route("/", Method::GET, index)
-    //             .route("/day-{day}", Method::GET, index)
-    //             .route("/grid-{day}", Method::GET, index)
-    //             .route("/students", Method::GET, students)
-    //             .route("/sections", Method::GET, index)
-    //             .route("/teams", Method::GET, index)
-
-    //             .route("/students", Method::POST, change_student)
-    //             .route("/students", Method::POST, post_student_error)
-    //             .route("/students", Method::POST, new_student)
-    //             .resource("/hello/{name}/{apples}/index.html",
-    //                       |r| r.method(Method::GET).with(hello))
-    //             )
-    //     .bind("127.0.0.1:8088")
-    //     .unwrap()
-    //     .run();
 }
